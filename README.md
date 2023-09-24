301aeaf9a14a32a577b7e8b3feb50c0f8824eb81
Z <zeta.hoffman@protonmail.com>
2023-09-24 16:28:45 -0600

    release

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # Image-Art
- IA-Project1
+ IA-Project1 - Image Art, ITCR, 2023
+
+# How to run the program
+## 1. Install dependencies
+```haskell
+pip install -r requirements.txt
+```
+## 2. Helo & Usage
+```haskell
+usage: main.py [-h] -i INPUT -o OUTPUT [-t]
+
+Image processing script
+
+options:
+  -h, --help            show this help message and exit
+  -i INPUT, --input INPUT
+                        Input image file path
+  -o OUTPUT, --output OUTPUT
+                        Output image file name
+  -t, --time            Print the start time and end time with timestap
+  ```
+## 2. Run the program
+
+```haskell
+python main.py -i <image input> -o <image result> -t
+```
+## 3. Example
+```haskell
+python main.py -i images/saber2.png -o saber2_result.png -t
+```
+## 4. Results
+### 4.1 Original image
+![alt text](img/saber2.jpg)
+<p><center>Figure 1. Original image</center></p>
+
+![alt text](img/result_saber2_fitness2.jpg)
+<p><center>Figure 2. Result image using the 2nd fitness implementation</center></p>
---
+++ main.py
@@ -0,0 +1,230 @@
+#!/usr/lcal/bin/env python
+# -*- coding: utf-8 -*-
+
+#region Imports
+import cv2
+import argparse
+import matplotlib.pyplot as plt
+from PIL import Image, ImageDraw
+from math import ceil
+from typing import Final
+import random
+import time
+#endregion
+
+# This code implements a genetic algorithm to solve the following problem:
+#
+#   * Given a grid of dimensions GRID_DIMENSION x GRID_DIMENSION, place
+#     NUM_CHROMOSOMES queens on the grid such that no two queens can attack
+#     each other (i.e., they cannot share the same row, column, or diagonal).
+#
+# This code is a modified version of the code found at
+
+
+#region Constants
+GRID_DIMENSION: Final[int]= 6
+NUM_CHROMOSOMES: Final[int] = 90
+MUTATION_PROBABILITY: Final[float] = 0.5
+CROSSOVER_CHANCE: Final[float] = 0.3
+#endregion
+
+#region fitness
+# fitness calculating function
+""" The code above does the following:
+1. Calculates the mean values of each color channel of the source image within the boundary defined by the chromosome.
+2. Calculates the distance between each color channel of the chromosome and the mean values of the source image.
+3. Returns the inverse of the distance as a fitness score. """
+
+""" def fitness(chromosome: list[int], coord: tuple, source_img: Image)  -> float: #fitness v1
+
+    # calculate mean values on each channel of rgb scheme
+    means = source_img[coord[1]:coord[3], coord[0]:coord[2]].mean(axis=(0,1))
+
+    # calculate 3d distance squared between chromosome and mean values
+    score:float = 0.0
+    for i in range(len(chromosome)):
+        score += (chromosome[i] / means[i]) * (chromosome[i] / means[i])
+    
+    # return fitness score
+    return 1 / (1 + score) """
+""" def fitness(chromosome: list[int], coord: tuple, source_img: Image)  -> float: #fitness v2
+
+    # calculate mean values on each channel of rgb scheme
+    means = source_img[coord[1]:coord[3], coord[0]:coord[2]].mean(axis=(0,1))
+
+    # calculate 3d distance squared between chromosome and mean values
+    score:float = 0.0
+    for i in range(len(chromosome)):
+        score += (chromosome[i] / means[i]) ** (chromosome[i] / means[i])
+    
+    # return fitness score
+    return 1 / (1 + score) """
+def fitness(chromosome: list[int], coord: tuple, source_img: Image)  -> float: #fitness v3
+
+    # calculate mean values on each channel of rgb scheme
+    means = source_img[coord[1]:coord[3], coord[0]:coord[2]].mean(axis=(0,1))
+
+    # calculate 3d distance squared between chromosome and mean values
+    score:float = 0.0
+    for i in range(len(chromosome)):
+        score += (chromosome[i] - means[i]) * (chromosome[i] - means[i])
+    
+    # return fitness score
+    return 1 / (1 + score)
+#endregion
+
+#region selection
+def selection(population: list, probability: list) -> list:
+    """ The code above does the following:
+    1. Calculate the sum of all probabilities
+    2. Calculate the probability to be selected in next generation
+    3. Calculate cumulative probability distribution
+    4. Select the chromosomes according to cumulative probability distribution """
+    # calculate the sum of all probabilities
+    total = 0.0
+    for i in range(NUM_CHROMOSOMES):
+        total += probability[i]
+
+    # calculate the probability to be selected in next generation
+    for i in range(NUM_CHROMOSOMES):
+        probability[i] /= total # normalize the probability
+
+    # generate new array for population constructed from selected chromosomes
+    new_population: list[None] = [None] * NUM_CHROMOSOMES
+
+    # calculate cumulative probability distribution
+    cumulative_probability: list[None] = [None] * NUM_CHROMOSOMES
+    cumulative_probability[0] = probability[0]
+    for i in range(1,NUM_CHROMOSOMES):
+        cumulative_probability[i] = probability[i] + cumulative_probability[i-1]
+
+    # select the chromosomes according to cumulative probability distribution
+    for i in range(NUM_CHROMOSOMES):
+        r: float = random.uniform(0,1)
+        for j in range(NUM_CHROMOSOMES):
+            if j == 0:
+                if 0 <= r and r < cumulative_probability[j]:
+                    new_population[i] = population[j].copy()
+            else:
+                if cumulative_probability[j-1] <= r and r < cumulative_probability[j]:
+                    new_population[i] = population[j].copy()
+
+    return new_population
+#endregion
+
+#region crossover
+def crossover(population: list) -> list:
+    """ The code above does the following:
+    1. We choose parents for crossover (randomly, according to CROSSOVER_CHANCE)
+    2. We choose a random cut point
+    3. We apply one-cut crossover technique to chosen parents """
+    parents:list = [] # list of parents
+    for i in range(NUM_CHROMOSOMES):
+        r: float = random.uniform(0,1) # generate random number between 0 and 1
+        if r < CROSSOVER_CHANCE:
+            parents.append(i) # add the chromosome to the list of parents
+    for i in range(len(parents)):
+        cut = random.randint(1,3) # generate random cut point
+        for k in range(cut, len(population[parents[i]])):
+            population[parents[i]][k] = population[parents[(i + 1)%(i + 1)]][k] # apply one-cut crossover technique to chosen parents
+
+    return population
+#endregion
+
+#region mutation
+def mutation(population: list) -> list:
+    """ The code above does the following:
+    1. The population is a list of integers, where each integer is a move in the game.
+    2. We go through every chromosome in the population.
+    3. We go through every move in the chromosome.
+    4. We generate a random number r between 0 and 1.
+    5. If r is less than MUTATION_PROBABILITY, we change the move to a random number between 0 and 256.
+    6. We return the new population. """
+    for chromosome in population:
+        for move in range(len(chromosome)):
+            r: float = random.uniform(0, 1) # generate random number between 0 and 1
+            if r < MUTATION_PROBABILITY:
+                chromosome[move] = random.randint(0,256) # change the move to a random number between 0 and 2568
+
+    return population
+#endregion
+
+#region gets
+def getChromosome(coord: tuple, source_img: Image)  -> list[list[int]]:
+    """ The code above does the following:
+    1. Generates a population of chromosomes (RGB color values)
+    2. Calculates the fitness score for each chromosome
+    3. Selects the best chromosome
+    4. Crossover
+    5. Mutation
+    6. Repeat 2-5 until the best chromosome is good enough """
+    # generate population of chromosomes
+    population: list[list[int]] = [[random.randint(0,256), random.randint(0,256), random.randint(0,256)].copy() for i in range(NUM_CHROMOSOMES)] # generate random chromosomes
+    score: list[None] = [None] * NUM_CHROMOSOMES # list of fitness scores for each chromosome
+    best_score : float = 0.0 # best score of the best chromosome
+    while(best_score < 0.1):
+        for i in range(NUM_CHROMOSOMES):
+            score[i] = fitness(chromosome=population[i], coord=coord, source_img=source_img) # calculate fitness score for each chromosome
+            if score[i] > best_score:
+                best_score = score[i] # update the best score
+                best_chromosome: list[int] = population[i] # update the best chromosome
+        
+        population = selection(population=population, probability=score) # select the best chromosomes
+        population = crossover(population=population) # run the crossover procedure
+        population = mutation(population=population) # run the mutation procedure
+
+    return best_chromosome
+#endregion
+
+#region main
+def main() -> None:
+    """ The code above does the following:
+    1. Read the image and convert it to RGB
+    2. Make the image divisible by squares of pixels with GRID_DIMENSION
+    3. Create an empty image of the same size
+    4. For each square, find the best chromosome and draw a circle with the appropriate color on the image """
+    parser = argparse.ArgumentParser(description='Image processing script')
+    parser.add_argument('-i', '--input', required=True, help='Input image file path')
+    parser.add_argument('-o', '--output', required=True, help='Output image file name')
+    parser.add_argument('-t', '--time', action='store_true', help='Print the start time and end time with timestap')
+    args: argparse.Namespace = parser.parse_args()
+
+    start_time: float = time.time() # start timing
+    if args.time:
+        print(f'Start time: {time.ctime(start_time)}')
+    image = cv2.imread(f'./img/{args.input}') # read the image
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # convert the image to RGB
+
+    # add some pixels by reflecting the image to make it divisible by squares of pixels with GRID_DIMENSION
+    height, width, _ = image.shape # get the height and width of the image
+    height_offset: int = ceil(height / GRID_DIMENSION) * GRID_DIMENSION - height # calculate the number of pixels to add to the height
+    width_offset: int = ceil(width / GRID_DIMENSION) * GRID_DIMENSION - width # calculate the number of pixels to add to the width
+    image = cv2.copyMakeBorder(src=image, top=0, bottom=height_offset, left=0, right=width_offset, borderType=cv2.BORDER_REFLECT) # add the pixels
+    height, width, _ = image.shape # get the height and width of the image
+
+    im: Image = Image.new(mode='RGB', size=(width, height), color='black') # create an empty image of the same size
+    draw: ImageDraw = ImageDraw.Draw(im=im) # create a drawing object
+
+    for y in range(0, height, GRID_DIMENSION): 
+        for x in range(0, width, GRID_DIMENSION):
+            # find the best chromosome for square with coordinates (x,y) and (x + GRID_DIMENSION, y + GRID_DIMENSION)  
+            solution: list[list[int]] = getChromosome(coord=(x, y, x + GRID_DIMENSION, y + GRID_DIMENSION), source_img=image)
+            # draw that the most appropriate chromosome on the image with form of circle
+            draw.ellipse((x, y, x + GRID_DIMENSION, y + GRID_DIMENSION), fill=(solution[0], solution[1], solution[2]))
+
+    # end timing and print it to the console
+    if args.time:
+        print(f'End time: {time.ctime(time.time())}')
+    
+    print(f'Elapsed time: {time.time() - start_time} seconds')
+
+    # show the generated image
+    plt.imshow(im)
+    plt.show()
+    # save the image
+    im.save(f'./img/{args.output}')   
+#endregion
+
+
+if __name__ == '__main__':
+    main()
